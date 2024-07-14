a63891119b3f5a4c8849f0440005160298945726
kadir2 <35632225+kadir2@users.noreply.github.com>
2024-07-14 17:55:04 +0300

    Create 2_string.cpp

---
+++ 2_string.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+string str1 = "birincisi bu";
+string str2 = "ikinci bu";
+string str3 =str1+str2;
+string str4 ;
+
+cin >> str4; //boşluk karakterinde durur
+cout << str1<<endl <<str2<<endl<<str3<<endl<<str4;
+
+
+return 0;
+}

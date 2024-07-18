418730de2575e19802e92790c0cee3ceff8c973b
kadir2 <35632225+kadir2@users.noreply.github.com>
2024-07-18 15:49:51 +0300

    Create 10_func_overlodaing.cpp

---
+++ 10_func_overlodaing.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+void mrb(){
+
+cout<< "merhabalar"<<endl;
+
+}
+
+void mrb(string ax){
+  cout << "selam " << ax <<endl; 
+}
+int main(){
+
+mrb();
+
+mrb("test yazisi");
+
+  return 0;
+}

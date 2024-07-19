0d9984917ee2426f5c7a8e77d9195d48bf472118
kadir2 <35632225+kadir2@users.noreply.github.com>
2024-07-19 14:51:34 +0300

    Create calisan.h

---
+++ class_2/calisan.h
@@ -0,0 +1,12 @@
+#include "iostream"
+using namespace std;
+
+class Calisan{
+public:
+    Calisan(int = 0, int = 0, string = "isimsiz");
+    void selam();
+private:
+    int id;
+    string isim;
+    int maas;
+};

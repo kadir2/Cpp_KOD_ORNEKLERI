8cbbdcdf746a079a98ac1f7983e8ae19e55d39d4
kadir2 <35632225+kadir2@users.noreply.github.com>
2024-07-19 21:38:51 +0300

    Create calisan.h

---
+++ class_3/calisan.h
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+class personeller {
+private:
+    string name;
+    int age;
+    char gender;
+    int salary;
+
+public:
+    void pin(string ="belirsiz", int =00, char ='x',int =000);
+    void find();
+};
+
